f403d5973c421a377972995460806a8f6a468c32
mirobili <mirobili@mail.bg>
2025-10-24 16:14:42 +0300

    init

---
+++ src/main/java/net/biliarski/razhodi/RazhodiApplication.java
@@ -0,0 +1,13 @@
+package net.biliarski.razhodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazhodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RazhodiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/biliarski/razhodi/RazhodiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.biliarski.razhodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazhodiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
